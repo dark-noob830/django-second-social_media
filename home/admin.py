35a1ee93358862c1701a57d128333aa3ec5459eb
mahdi <mahdiamr83@gmail.com>
2024-10-19 22:23:02 +0330

    account complete post delete and display complete

---
+++ home/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'slug', 'updated']
+    search_fields = ('slug',)
+    list_filter = ['updated']
+    prepopulated_fields = {'slug': ('body',)}
+    raw_id_fields = ('user',)
+
+
+admin.site.register(Post, PostAdmin)
